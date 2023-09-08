69ccc5a56472667c8de2b1aa553e10f9d4304344
Sourab Mangrulkar <13534540+pacman100@users.noreply.github.com>
2023-09-08 12:47:26 +0530

    adding `chat-intstruct-mixer` data notebook, major reformatting and fixes

---
+++ chat_assistant/training/train.py
@@ -0,0 +1,235 @@
+# coding=utf-8
+# Copyright 2023 The HuggingFace Inc. team. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+from dataclasses import dataclass, field
+import os
+import subprocess
+from typing import Optional
+
+from transformers import HfArgumentParser, TrainingArguments, Trainer
+from utils import *
+
+########################################################################
+# This is a fully working simple example to use trl's RewardTrainer.
+#
+# This example fine-tunes any causal language model (GPT-2, GPT-Neo, etc.)
+# by using the RewardTrainer from trl, we will leverage PEFT library to finetune
+# adapters on the model.
+#
+########################################################################
+
+
+# Define and parse arguments.
+@dataclass
+class ScriptArguments:
+    """
+    These arguments vary depending on how many GPUs you have, what their capacity and features are, and what size model you want to train.
+    """
+
+    local_rank: Optional[int] = field(default=-1, metadata={"help": "Used for multi-gpu"})
+
+    per_device_train_batch_size: Optional[int] = field(default=4)
+    per_device_eval_batch_size: Optional[int] = field(default=1)
+    gradient_accumulation_steps: Optional[int] = field(default=4)
+    learning_rate: Optional[float] = field(default=2e-4)
+    max_grad_norm: Optional[float] = field(default=0.3)
+    weight_decay: Optional[float] = field(default=0.001)
+    lora_alpha: Optional[int] = field(default=16)
+    lora_dropout: Optional[float] = field(default=0.1)
+    lora_r: Optional[int] = field(default=64)
+    lora_target_modules: Optional[str] = field(
+        default="q_proj,k_proj,v_proj,o_proj,down_proj,up_proj,gate_proj",
+        metadata={"help": "comma separated list of target modules to apply LoRA layers to"},
+    )
+    max_seq_length: Optional[int] = field(default=512)
+    model_name: Optional[str] = field(
+        default="Salesforce/codegen25-7b-multi",
+        metadata={
+            "help": "The model that you want to train from the Hugging Face hub. E.g. gpt2, gpt2-xl, bert, etc."
+        },
+    )
+    dataset_name: Optional[str] = field(
+        default="timdettmers/openassistant-guanaco",
+        metadata={"help": "The preference dataset to use."},
+    )
+    use_4bit: Optional[bool] = field(
+        default=True,
+        metadata={"help": "Activate 4bit precision base model loading"},
+    )
+    use_nested_quant: Optional[bool] = field(
+        default=False,
+        metadata={"help": "Activate nested quantization for 4bit base models"},
+    )
+    bnb_4bit_compute_dtype: Optional[str] = field(
+        default="float16",
+        metadata={"help": "Compute dtype for 4bit base models"},
+    )
+    bnb_4bit_quant_type: Optional[str] = field(
+        default="nf4",
+        metadata={"help": "Quantization type fp4 or nf4"},
+    )
+    num_train_epochs: Optional[int] = field(
+        default=1,
+        metadata={"help": "The number of training epochs for the reward model."},
+    )
+    fp16: Optional[bool] = field(
+        default=False,
+        metadata={"help": "Enables fp16 training."},
+    )
+    bf16: Optional[bool] = field(
+        default=False,
+        metadata={"help": "Enables bf16 training."},
+    )
+    packing: Optional[bool] = field(
+        default=False,
+        metadata={"help": "Use packing dataset creating."},
+    )
+    gradient_checkpointing: Optional[bool] = field(
+        default=True,
+        metadata={"help": "Enables gradient checkpointing."},
+    )
+    optim: Optional[str] = field(
+        default="paged_adamw_32bit",
+        metadata={"help": "The optimizer to use."},
+    )
+    lr_scheduler_type: str = field(
+        default="constant",
+        metadata={"help": "Learning rate schedule. Constant a bit better than cosine, and has advantage for analysis"},
+    )
+    max_steps: int = field(default=10000, metadata={"help": "How many optimizer update steps to take"})
+    warmup_ratio: float = field(default=0.03, metadata={"help": "Fraction of steps to do a warmup for"})
+    save_steps: int = field(default=10, metadata={"help": "Save checkpoint every X updates steps."})
+    eval_steps: int = field(default=10, metadata={"help": "Eval model every X steps."})
+    logging_steps: int = field(default=10, metadata={"help": "Log every X updates steps."})
+    output_dir: str = field(default="results", metadata={"help": "Where to store the final model."})
+    use_flash_attn: Optional[bool] = field(
+        default=False,
+        metadata={"help": "Enables Flash attention for training."},
+    )
+    use_peft_lora: Optional[bool] = field(
+        default=False,
+        metadata={"help": "Enables PEFT LoRA for training."},
+    )
+    use_8bit_qunatization: Optional[bool] = field(
+        default=False,
+        metadata={"help": "Enables loading model in 8bit."},
+    )
+    use_4bit_qunatization: Optional[bool] = field(
+        default=False,
+        metadata={"help": "Enables loading model in 4bit."},
+    )
+    use_gradient_checkpointing: Optional[bool] = field(
+        default=False,
+        metadata={"help": "Enables Gradient Checkpointing."},
+    )
+    dataset_text_field: str = field(default="text", metadata={"help": "Dataset field to use as input text."})
+    push_to_hub: Optional[bool] = field(
+        default=False,
+        metadata={"help": "If True, pushes the model to the HF Hub"},
+    )
+    debug: Optional[bool] = field(
+        default=False,
+        metadata={"help": "If True, tests things like proper saving/loading/logging of model"},
+    )
+
+
+def main(args):
+    # training arguments
+    is_deepspeed_peft_enabled = (
+        os.environ.get("ACCELERATE_USE_DEEPSPEED", "False").lower() == "true" and args.use_peft_lora
+    )
+    save_strategy = "no" if is_deepspeed_peft_enabled else "steps"
+    training_arguments = TrainingArguments(
+        output_dir=args.output_dir,
+        per_device_train_batch_size=args.per_device_train_batch_size,
+        gradient_accumulation_steps=args.gradient_accumulation_steps,
+        optim=args.optim,
+        learning_rate=args.learning_rate,
+        fp16=args.fp16,
+        bf16=args.bf16,
+        max_grad_norm=args.max_grad_norm,
+        warmup_ratio=args.warmup_ratio,
+        lr_scheduler_type=args.lr_scheduler_type,
+        num_train_epochs=args.num_train_epochs,
+        evaluation_strategy="steps",
+        save_strategy=save_strategy,
+        max_steps=args.max_steps,
+        eval_steps=args.eval_steps,
+        save_steps=args.save_steps,
+        logging_steps=args.logging_steps,
+        push_to_hub=args.push_to_hub,
+        gradient_checkpointing=args.use_gradient_checkpointing,
+    )
+
+    # datasets
+    train_dataset, eval_dataset = create_datasets(tokenizer, args)
+
+    # model
+    model, peft_config, tokenizer = create_and_prepare_model(args)
+    model.config.use_cache = False
+
+    # trainer
+    trainer = Trainer(model=model, args=training_arguments, train_dataset=train_dataset, eval_dataset=eval_dataset)
+    trainer.accelerator.print(f"{trainer.model}")
+    if args.use_peft_lora:
+        trainer.model.print_trainable_parameters()
+
+    if is_deepspeed_peft_enabled:
+        trainer.add_callback(SaveDeepSpeedPeftModelCallback(trainer, save_steps=args.save_steps))
+
+    if args.use_peft_lora:
+        peft_module_casting_to_bf16(trainer.model, args)
+
+    # train
+    trainer.train()
+
+    # saving final model
+    if trainer.is_fsdp_enabled:
+        trainer.accelerator.state.fsdp_plugin.set_state_dict_type("FULL_STATE_DICT")
+
+    if is_deepspeed_peft_enabled:
+        trainer.accelerator.wait_for_everyone()
+        unwrapped_model = trainer.accelerator.unwrap_model(trainer.deepspeed)
+        unwrapped_model.save_pretrained(
+            args.output_dir, state_dict=trainer.accelerator.get_state_dict(trainer.deepspeed)
+        )
+        trainer.accelerator.wait_for_everyone()
+    else:
+        if args.push_to_hub:
+            trainer.push_to_hub()
+            if args.use_peft_lora:
+                trainer.model.push_to_hub(args.output_dir)
+        else:
+            trainer.save_model(args.output_dir)
+
+    # Save everything else on main process
+    if trainer.args.process_index == 0:
+        print("Sharding model if >10GB...")
+        # FSDP/DeepSpeed save the model as a single `pytorch_model.bin` file, so we need to shard it.
+        # We run this in a subprocess to avoid interference from the accelerators.
+        subprocess.run(
+            [
+                "python",
+                "shard_checkpoint.py",
+                f"--output_dir={args.output_dir}",
+            ],
+            check=True,
+        )
+        os.remove(os.path.join(args.output_dir, "training_args.bin"))
+
+
+if __name__ == "__main__":
+    parser = HfArgumentParser(ScriptArguments)
+    args = parser.parse_args_into_dataclasses()[0]
+    main(args)
---
+++ chat_assistant/training/utils.py
@@ -0,0 +1,236 @@
+import random
+import torch
+from transformers import TrainerCallback, TrainingArguments, TrainerState, TrainerControl
+from torch.utils.data import IterableDataset
+from datasets import load_dataset
+from tqdm import tqdm
+import warnings
+from peft import LoraConfig, get_peft_model
+from peft.tuners.lora import LoraLayer
+from transformers import (
+    AutoModelForCausalLM,
+    AutoTokenizer,
+    BitsAndBytesConfig,
+    AutoTokenizer,
+    TrainingArguments,
+)
+
+
+class SaveDeepSpeedPeftModelCallback(TrainerCallback):
+    def __init__(self, trainer, save_steps=500):
+        self.trainer = trainer
+        self.save_steps = save_steps
+
+    def on_step_end(
+        self,
+        args: TrainingArguments,
+        state: TrainerState,
+        control: TrainerControl,
+        **kwargs,
+    ):
+        if (state.global_step + 1) % self.save_steps == 0:
+            self.trainer.accelerator.wait_for_everyone()
+            unwrapped_model = self.trainer.accelerator.unwrap_model(self.trainer.deepspeed)
+            unwrapped_model.save_pretrained(
+                args.output_dir, state_dict=self.trainer.accelerator.get_state_dict(self.trainer.deepspeed)
+            )
+        return control
+
+
+class ConstantLengthDataset(IterableDataset):
+    """
+    Iterable dataset that returns constant length chunks of tokens from stream of text files.
+        Args:
+            tokenizer (Tokenizer): The processor used for proccessing the data.
+            dataset (dataset.Dataset): Dataset with text files.
+            infinite (bool): If True the iterator is reset after dataset reaches end else stops.
+            seq_length (int): Length of token sequences to return.
+            num_of_sequences (int): Number of token sequences to keep in buffer.
+            chars_per_token (int): Number of characters per token used to estimate number of tokens in text buffer.
+            shuffle (bool): If true, the samples in each buffer are suffled. Default is `True`.
+            add_eos_token (bool): If true, each buffer is delimited with eos token. Default is `True`.
+    """
+
+    def __init__(
+        self,
+        tokenizer,
+        dataset,
+        infinite=False,
+        seq_length=1024,
+        num_of_sequences=1024,
+        chars_per_token=3.6,
+        content_field="content",
+        shuffle=True,
+        add_eos_token=True,
+    ):
+        self.tokenizer = tokenizer
+        self.concat_token_id = tokenizer.eos_token_id
+        self.dataset = dataset
+        self.seq_length = seq_length
+        self.infinite = infinite
+        self.current_size = 0
+        self.max_buffer_size = seq_length * chars_per_token * num_of_sequences
+        self.content_field = content_field
+        self.shuffle = shuffle
+        self.add_eos_token = add_eos_token
+
+    def __iter__(self):
+        iterator = iter(self.dataset)
+        more_examples = True
+        while more_examples:
+            buffer, buffer_len = [], 0
+            while True:
+                if buffer_len >= self.max_buffer_size:
+                    break
+                try:
+                    buffer.append(next(iterator)[self.content_field])
+                    buffer_len += len(buffer[-1])
+                except StopIteration:
+                    if self.infinite:
+                        iterator = iter(self.dataset)
+                    else:
+                        more_examples = False
+                        break
+            tokenized_inputs = self.tokenizer(buffer, truncation=False)["input_ids"]
+            all_token_ids = tokenized_inputs
+            if self.add_eos_token:
+                all_token_ids = []
+                for tokenized_input in tokenized_inputs:
+                    all_token_ids.extend(tokenized_input + [self.concat_token_id])
+            examples = []
+            for i in range(0, len(all_token_ids), self.seq_length):
+                input_ids = all_token_ids[i : i + self.seq_length]
+                if len(input_ids) == self.seq_length:
+                    examples.append(input_ids)
+            if self.shuffle:
+                random.shuffle(examples)
+            for example in examples:
+                self.current_size += 1
+                yield {
+                    "input_ids": torch.LongTensor(example),
+                    "labels": torch.LongTensor(example),
+                }
+
+
+def chars_token_ratio(dataset, tokenizer, data_column, nb_examples=400):
+    """
+    Estimate the average number of characters per token in the dataset.
+    """
+    total_characters, total_tokens = 0, 0
+    for _, example in tqdm(zip(range(nb_examples), iter(dataset)), total=nb_examples):
+        total_characters += len(example[data_column])
+        total_tokens += len(tokenizer(example[data_column]).tokens())
+
+    return total_characters / total_tokens
+
+
+def create_datasets(tokenizer, args):
+    dataset = load_dataset(args.dataset_name, use_auth_token=True, num_proc=args.num_workers)
+    train_data = dataset["train"]
+    valid_data = dataset["test"]
+    print(f"Size of the train set: {len(train_data)}. Size of the validation set: {len(valid_data)}")
+    chars_per_token = chars_token_ratio(train_data, tokenizer, args.data_column)
+    print(f"The character to token ratio of the dataset is: {chars_per_token:.2f}")
+    train_dataset = ConstantLengthDataset(
+        tokenizer,
+        train_data,
+        infinite=True,
+        seq_length=args.seq_length,
+        chars_per_token=chars_per_token,
+        content_field=args.data_column,
+        shuffle=True,
+        add_eos_token=False,
+    )
+    valid_dataset = ConstantLengthDataset(
+        tokenizer,
+        valid_data,
+        infinite=False,
+        seq_length=args.seq_length,
+        chars_per_token=chars_per_token,
+        content_field=args.data_column,
+        shuffle=False,
+        add_eos_token=False,
+    )
+
+    return train_dataset, valid_dataset
+
+
+def create_and_prepare_model(args):
+    if args.use_flash_attn:
+        warnings.warn(
+            "Flash V2 support implemented here ignores padding/attention_mask/custom_mask. \n"
+            + "It is meant for continued pre-training with packing inputs to consume the entire sequence lengths."
+        )
+        from starcoder_flash_attn_monkey_patch import replace_starcoder_attn_with_flash_attn
+        from llama_flash_attn_monkey_patch import replace_llama_attn_with_flash_attn
+        from falcon_flash_attn_monkey_patch import replace_falcon_attn_with_flash_attn
+
+        replace_starcoder_attn_with_flash_attn()
+        replace_llama_attn_with_flash_attn()
+        replace_falcon_attn_with_flash_attn()
+    device_map = None
+    bnb_config = None
+    load_in_8bit = args.use_8bit_qunatization
+
+    if args.use_4bit_qunatization:
+        compute_dtype = getattr(torch, args.bnb_4bit_compute_dtype)
+
+        bnb_config = BitsAndBytesConfig(
+            load_in_4bit=args.use_4bit_qunatization,
+            bnb_4bit_quant_type=args.bnb_4bit_quant_type,
+            bnb_4bit_compute_dtype=compute_dtype,
+            bnb_4bit_use_double_quant=args.use_nested_quant,
+        )
+
+        if compute_dtype == torch.float16 and args.use_4bit_qunatization:
+            major, _ = torch.cuda.get_device_capability()
+            if major >= 8:
+                print("=" * 80)
+                print("Your GPU supports bfloat16, you can accelerate training with the argument --bf16")
+                print("=" * 80)
+
+    if args.use_4bit_qunatization or args.use_8bit_qunatization:
+        device_map = "auto"  # {"": 0}
+
+    model = AutoModelForCausalLM.from_pretrained(
+        args.model_name,
+        load_in_8bit=load_in_8bit,
+        quantization_config=bnb_config,
+        device_map=device_map,
+        use_cache=not args.use_gradient_checkpointing,
+        trust_remote_code=True,
+    )
+
+    peft_config = None
+    if args.use_peft_lora:
+        peft_config = LoraConfig(
+            lora_alpha=args.lora_alpha,
+            lora_dropout=args.lora_dropout,
+            r=args.lora_r,
+            bias="none",
+            task_type="CAUSAL_LM",
+            target_modules=args.lora_target_modules.split(","),
+        )
+        if args.use_gradient_checkpointing:
+            model.gradient_checkpointing_enable()
+
+        model = get_peft_model(model, peft_config)
+        model.print_trainable_parameters()
+
+    tokenizer = AutoTokenizer.from_pretrained(args.model_name, trust_remote_code=True)
+    tokenizer.pad_token = tokenizer.eos_token
+
+    return model, peft_config, tokenizer
+
+
+def peft_module_casting_to_bf16(model, args):
+    for name, module in model.named_modules():
+        if isinstance(module, LoraLayer):
+            if args.bf16:
+                module = module.to(torch.bfloat16)
+        if "norm" in name:
+            module = module.to(torch.float32)
+        if any(x in name for x in ["lm_head", "embed_tokens", "wte", "wpe"]):
+            if hasattr(module, "weight"):
+                if args.bf16 and module.weight.dtype == torch.float32:
+                    module = module.to(torch.bfloat16)
