797a0af11cd49b5dcc76de65f53069fef9a7b197
Sourab Mangrulkar <13534540+pacman100@users.noreply.github.com>
2023-10-20 09:50:07 -0700

    adding launch commands

--- personal_copilot/training/run_peft.sh
+++ personal_copilot/training/run_peft.sh
@@ -6,28 +6,27 @@ python train.py \
     --split "train" \
     --seq_length 2048 \
     --max_steps 2000 \
-    --batch_size 8 \
-    --gradient_accumulation_steps 2 \
-    --learning_rate 2e-4 \
-    --lr_scheduler_type "constant" \
+    --batch_size 4 \
+    --gradient_accumulation_steps 4 \
+    --learning_rate 5e-4 \
+    --lr_scheduler_type "cosine" \
     --weight_decay 0.01 \
     --num_warmup_steps 30 \
     --eval_freq 100 \
     --save_freq 100 \
     --log_freq 25 \
-    --push_to_hub \
     --num_workers 4 \
     --bf16 \
     --no_fp16 \
-    --output_dir "peft-lora-starcoder-personal-copilot-A100-40GB-colab" \
+    --output_dir "peft-lora-starcoder15B-v2-personal-copilot-A100-40GB-colab" \
     --fim_rate 0.5 \
     --fim_spm_rate 0.5 \
-    --use_peft_lora \
-    --lora_r 8 \
-    --lora_alpha 32 \
-    --lora_dropout 0.1 \
-    --lora_target_modules "c_proj,c_attn,q_attn,c_fc,c_proj" \
     --use_flash_attn \
+    --use_peft_lora \
+    --lora_r 32 \
+    --lora_alpha 64 \
+    --lora_dropout 0.0 \
+    --lora_target_modules "c_proj,c_attn,q_attn,c_fc,c_proj" \
     --use_4bit_qunatization \
     --use_nested_quant \
     --bnb_4bit_compute_dtype "bfloat16"
