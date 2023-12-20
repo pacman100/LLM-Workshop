2e1a69203ab456be704df514e67604ef2f3b90bc
Sourab Mangrulkar <13534540+pacman100@users.noreply.github.com>
2023-12-20 21:26:25 +0530

    fixes

--- chat_assistant/training/run_deepspeed.sh
+++ chat_assistant/training/run_deepspeed.sh
@@ -21,11 +21,11 @@ accelerate launch --config_file "configs/deepspeed_config.yaml"  train.py \
 --learning_rate 2e-5 \
 --lr_scheduler_type "cosine" \
 --weight_decay 1e-4 \
---warmup_ratio 0.01 \
+--warmup_ratio 0.0 \
 --max_grad_norm 1.0 \
 --output_dir "mistral-sft-ds" \
---per_device_train_batch_size 4 \
---gradient_accumulation_steps 8 \
+--per_device_train_batch_size 2 \
+--gradient_accumulation_steps 16 \
 --gradient_checkpointing False \
 --dataset_text_field "content" \
 --use_flash_attn True
