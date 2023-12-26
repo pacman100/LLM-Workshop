095461bf73f0db837341887a3a1782b87b5f9d39
Sourab Mangrulkar <13534540+pacman100@users.noreply.github.com>
2023-12-26 15:59:30 +0530

    Making FSDP in sync with DeepSpeed

--- chat_assistant/training/run_fsdp.sh
+++ chat_assistant/training/run_fsdp.sh
@@ -24,8 +24,9 @@ accelerate launch --config_file "configs/fsdp_config.yaml"  train.py \
 --warmup_ratio 0.1 \
 --max_grad_norm 1.0 \
 --output_dir "mistral-sft-fsdp" \
---per_device_train_batch_size 2 \
---gradient_accumulation_steps 1 \
---gradient_checkpointing False \
+--per_device_train_batch_size 16 \
+--gradient_accumulation_steps 4 \
+--gradient_checkpointing True \
+--use_reentrant False \
 --dataset_text_field "content" \
 --use_flash_attn True
