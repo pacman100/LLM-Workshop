5a0d99b7c3d1f058d2180d55d3a50b93a8e3c2ab
Sourab Mangrulkar <13534540+pacman100@users.noreply.github.com>
2024-02-02 16:46:59 +0530

    reduce GPU memory usage from 30 GB to 15 GB by setting `use_reentrant=True`

--- chat_assistant/dpo/run_dpo.sh
+++ chat_assistant/dpo/run_dpo.sh
@@ -32,7 +32,7 @@ CUDA_VISIBLE_DEVICES=0 WANDB_PROJECT=rlhf python train.py \
 --per_device_eval_batch_size 4 \
 --gradient_accumulation_steps 8 \
 --gradient_checkpointing True \
---use_reentrant False \
+--use_reentrant True \
 --dataset_text_field "content" \
 --use_peft_lora True \
 --lora_r 8 \
