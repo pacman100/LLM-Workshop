fcdd846265f11811097d25048e192dbf3fb354e4
Sourab Mangrulkar <13534540+pacman100@users.noreply.github.com>
2024-02-12 16:02:47 +0530

    Update run_deepspeed.sh

--- personal_copilot/training/run_deepspeed.sh
+++ personal_copilot/training/run_deepspeed.sh
@@ -1,24 +1,33 @@
-accelerate launch --config_file "configs/deepspeed_config.yaml"  train.py \
---model_path "bigcode/starcoderbase-1b" \
---dataset_name "smangrul/hf-stack-v1"  \
---subset "data" \
---data_column "content" \
---split="train" \
---seq_length 2048 \
+accelerate launch --config_file "configs/deepspeed_config.yaml" train.py \
+--model_name_or_path "codellama/CodeLlama-7b-Instruct-hf" \
+--dataset_name "smangrul/hug_stack" \
+--splits "train" \
+--max_seq_len 2048 \
 --max_steps 2000 \
---batch_size 8 \
---gradient_accumulation_steps 2 \
---learning_rate 5e-5  \
+--save_steps 500 \
+--eval_steps 100 \
+--logging_steps 5 \
+--log_level "info" \
+--logging_strategy "steps" \
+--evaluation_strategy "steps" \
+--save_strategy "steps" \
+--push_to_hub \
+--hub_private_repo True \
+--hub_strategy "every_save" \
+--bf16 True \
+--learning_rate 2e-5 \
 --lr_scheduler_type "cosine" \
---weight_decay 0.01 \
---num_warmup_steps 30 \
---eval_freq 100 \
---save_freq 1000 \
---log_freq 25 \
---num_workers 4 \
---bf16 \
---no_fp16  \
---output_dir "full-finetune-starcoderbase-1b-deepspeed-colab" \
+--weight_decay 0.1 \
+--warmup_ratio 0.1 \
+--max_grad_norm 1.0 \
+--output_dir "codellama-hugcoder-df" \
+--per_device_train_batch_size 16 \
+--per_device_eval_batch_size 16 \
+--gradient_accumulation_steps 4 \
+--gradient_checkpointing True \
+--use_reentrant False \
+--dataset_text_field "text" \
+--test_size 0.1 \
 --fim_rate 0.5 \
 --fim_spm_rate 0.5 \
---use_flash_attn
+--use_flash_attn True
